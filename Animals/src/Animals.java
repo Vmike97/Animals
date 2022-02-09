06a98335d854f1dd52fa25d024ee45872ea6ce04
Vmike97 <46335610+Vmike97@users.noreply.github.com>
2022-02-09 16:21:33 -0500

    First commit adding Main class and Animals class

---
+++ Animals/src/Animals.java
@@ -0,0 +1,40 @@
+
+public class Animals {
+	public String name;
+	public int age;
+	public int weight;
+
+// Constructor not used as not all information can be available while creating Animal object. 
+	
+//	Animals(String name, int age, int weight){
+//		this.name = name;
+//		this.age = age;
+//		this.weight = weight;
+//	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	
+}
---
+++ Animals/src/Main.java
@@ -0,0 +1,33 @@
+//This project focuses on parent child classes (Parent; Animals) (Child; Dog, Cat, Eagle) using "extends" 
+//on child classes. Also uses Bird interface on Eagle class using "implements" to ensure Eagle 
+//has desired Bird interface methods. 
+
+public class Main {
+
+	public static void main(String[] args) {
+		Dog dog = new Dog();
+		dog.setName("Billy");
+		dog.setAge(5);
+		dog.setWeight(60);
+		dog.setRunSpeed(20);
+		System.out.println(dog);
+		
+		Cat cat = new Cat();
+		cat.setName("Mittens");
+		cat.setAge(5);
+		cat.setWeight(60);
+		cat.setSleepTime(360);
+		System.out.println(cat);
+		
+		Eagle eagle = new Eagle();
+		eagle.setAge(10);
+		eagle.setWeight(30);
+		eagle.setWingSpan(200);
+		eagle.setBeakColour("Orange");
+		System.out.println(eagle);
+		eagle.wingSpan();
+		eagle.beakColor();
+
+	}
+
+}
